d136ebc11808b933cb7247a46720aed81433badd
Tangzixia <ks2337768320@163.com>
2018-12-03 22:40:11 +0800

    Update data_aug_self.py

--- data_aug_self.py
+++ data_aug_self.py
@@ -61,6 +61,54 @@ def rotate_image_std(src, angle, scale=1.):
         src, rot_mat, (int(math.ceil(nw)), int(math.ceil(nh))),
         flags=cv2.INTER_LANCZOS4)
 
+def rotate_im(image, angle):
+    """Rotate the image.
+
+    Rotate the image such that the rotated image is enclosed inside the tightest
+    rectangle. The area not occupied by the pixels of the original image is colored
+    black.
+
+    Parameters
+    ----------
+
+    image : numpy.ndarray
+        numpy image
+
+    angle : float
+        angle by which the image is to be rotated
+
+    Returns
+    -------
+
+    numpy.ndarray
+        Rotated Image
+
+    """
+    # grab the dimensions of the image and then determine the
+    # centre
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    image = cv2.warpAffine(image, M, (nW, nH))
+
+    #    image = cv2.resize(image, (w,h))
+    return image
 
 def scale_image(img,scale):
     orig_h, orig_w = img.shape[:2]
